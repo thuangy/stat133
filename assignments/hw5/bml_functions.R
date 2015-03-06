b50318c8227f918521ea847959b7e876d1ccb9e1
Tina Huang <thuang@berkeley.edu>
2015-03-06 11:03:50 -0800

    BML Functions

--- assignments/hw5/bml_functions.R
+++ assignments/hw5/bml_functions.R
@@ -7,10 +7,10 @@
 ## Output : A matrix [m] with entries 0 (no cars) 1 (red cars) or 2 (blue cars)
 ## that stores the state of the system (i.e. location of red and blue cars)
 
-bml.init <- function(r, c, p){
-
-  
-   #return(m)
+bml.init = function(r, c, p){
+  car_nums = sample(0:2, r*c, replace=TRUE, prob=c(1-p, p/2, p/2))
+  m = matrix(car_nums, r, c)
+   return(m)
 }
 
 #### Function to move the system one step (east and north)
@@ -21,16 +21,93 @@ bml.init <- function(r, c, p){
 ## NOTE : the function should move the red cars once and the blue cars once,
 ## you can write extra functions that do just a step north or just a step east.
 
-bml.step <- function(m){
+#List of Rows
 
-  
-   #return(list(m, grid.new))
+as.list(data.frame(t(m)))
+
+#List of Columns
+
+as.list(data.frame(m))
+
+move_north = function(v){
+  i = 1
+  while(i <= length(v)){
+    if(v[i] == 2){
+      if(i == 1){
+        if(v[length(v)] == 0){
+          v[length(v)] = v[i]
+          v[i] = 0
+        }
+      }else{
+        if(v[i-1] == 0){
+          v[i-1] = v[i]
+          v[i] = 0
+        }
+      }
+      i = i + 2
+    }
+    i = i + 1
+  }
+  return(v)
 }
+
+move_east = function(v){
+  i = 1
+  while(i <= length(v)){
+    if(v[i] == 1){
+      if(i == length(v)){
+        if(v[1] == 0){
+          v[1] = v[i]
+          v[i] = 0
+        }
+      }else{
+        if(v[i+1] == 0){
+          v[i+1] = v[i]
+          v[i] = 0
+        }
+      }
+      i = i + 2
+    }
+    i = i + 1
+  }
+  return(v)
+}
+
+newm = sapply(as.list(data.frame(m)), move_north)
+sapply(as.list(data.frame(t(m))), move_east)
+
+rotate <- function(x) t(apply(x, 2, rev))
+
+
+bml.step <- function(m){
+  eastm = t(sapply(as.list(data.frame(t(m))), move_east))
+  newm = sapply(as.list(data.frame(eastm)), move_north)
+  if(all(newm == m)){
+    grid.new = FALSE
+  }else{
+    grid.new = TRUE
+  }
+  
+  return(list(newm, grid.new))
+}
+
 
 #### Function to do a simulation for a given set of input parameters
 ## Input : size of grid [r and c] and density [p]
 ## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
 
-bml.sim <- function(r, c, p){
-
+bml.sim <- function(r, c, p, max.it=5000){
+  mat = list(bml.init(r, c, p), TRUE)
+  i = 1
+  gridlock = FALSE
+  while(i < max.it){
+    mat = bml.step(mat[[1]])
+    
+    if(mat[[2]] == FALSE){
+      gridlock = TRUE
+      break}
+    i = i + 1
+  }
+  return(list(mat[[1]], i, gridlock))
 }
+
